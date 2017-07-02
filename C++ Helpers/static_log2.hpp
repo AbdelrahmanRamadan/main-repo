3d6acc0cada1ad59909d1c9fca134d3ecf452a9e
AbdelrahmanRamadan <abdelrahman0xff@gmail.com>
2017-07-02 18:06:40 +0200

    bbb

--- C++ Helpers/static_log2.hpp
+++
@@ -1,9 +0,0 @@
-
-template <long long N>
-struct static_log2 {
-	const static int value = 1 + static_log2<(N >> 1)>::value;
-};
-template <>
-struct static_log2 <0> {
-	const static int value = -1;
-};
